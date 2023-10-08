20b3bd7aa151fe2896cf1531a584cd90b861dd16
OST-Gh <108372693+OST-Gh@users.noreply.github.com>
2023-10-08 21:32:23 +0000

    Delete mods.tar.gz
    build update.
    
    build update.
    
    build update.
    
    build update.
    
    this' a pain in the ass

---
+++ build.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+export MOD="$HOME/mod/packs/garlic/mods"
+export STORY="$HOME/story/"
+
+cd $STORY
+if [ ! -d mods ]; then
+  mkdir mods
+else
+  rm -r mods/*
+fi
+
+for file in $MOD/*; do
+  ln -f $file $STORY/mods/
+done
+
+# private origin=$PWD
+# cd $STORY
+# tar --compress -cf mods.tar.gz mods
+# cd $origin
+
+git add -A
+git commit -qm "build update."
+git push -q
---
+++ update.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+export MOD="$HOME/mod/packs/garlic/mods"
+export STORY="$HOME/story/"
+
+git clone -q https://github.com/OST-Gh/story.git $STORY/clone
+
+mv -f $STORY/clone/mods $STORY
+
+update_scripts() {
+  if [[ ! $1 ]]; then; return; fi
+  if [ $(cat $STORY/clone/$1.zsh) != $(cat $STORY/$1.zsh)  ]; then
+    mv -f $STORY/clone/$1.zsh $STORY
+  fi
+}
+
+update_scripts update
+update_scripts build
+
+rm -rf clone
